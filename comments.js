60f68709c6a56785fbde24c56a1f0da6ae2fd6b3
Alex Coady <alexandercoady@me.com>
2013-08-05 23:38:02 +0100

    Added comment object

--- comments.js
+++ comments.js
@@ -6,6 +6,52 @@ fs.readFile("src/example.js", "utf8", function (err, fileContents) {
 		return;
 	}
 
+	var Comment = function (title, author, date, param, return_prop) {
+		this.title = title;
+		this.author = author;
+		this.date = date;
+		this.param = param;
+		this.return_prop = return_prop;
+	};
+
+	Comment.prototype = {
+
+		title: undefined,
+		author: undefined,
+		date: undefined,
+		param: undefined,
+		return_prop: undefined,
+
+		set: function setFn (property, value) {
+
+			this[property] = value;
+		},
+
+		get: function (property) {
+
+			return this[property];
+		},
+
+		isValid: function isValidFn () {
+
+			if (this.title || this.author || this.date || this.param || this.return_prop) {
+
+				return true;
+			}
+			return;
+		},
+
+		getTitle: function getTitleFn () {
+
+			return this.title;
+		},
+
+		printOut: function printOutFn () {
+
+			console.log(this);
+		}
+	}
+
 	var string = fileContents,
 		char_count = fileContents.length,
 		char_i = 0,
@@ -13,7 +59,7 @@ fs.readFile("src/example.js", "utf8", function (err, fileContents) {
 		comment_entered = false,
 		started_i = 0,
 		finished_i = 0,
-		comments = [];
+		comments_string_array = [];
 
 	for (char_i; char_i < char_count; char_i += 1) {
 
@@ -35,29 +81,32 @@ fs.readFile("src/example.js", "utf8", function (err, fileContents) {
 			char_i += 1;
 			finished_i = char_i;
 
-			comments.push( string.substring(started_i, finished_i + 1) );
+			comments_string_array.push( string.substring(started_i, finished_i + 1) );
 			continue;		
 		} 
 	}
 
-	var comments_count = comments.length,
+	var comments_count = comments_string_array.length,
 		comments_i = 0,
-		comment,
+		comment_string,
 		lines,
 		line_count,
 		line_i,
 		line,
-		kw_index = {};
+		kw_index = {},
+		comments = [];
 
 	console.log("Found", comments_count, "comments");
 
 	for (comments_i; comments_i < comments_count; comments_i += 1) {
 
-		comment = comments[comments_i];
+		comment_string = comments_string_array[comments_i];
 		// Split lines in the comment into seperate array items
-		lines = comment.match(/[^\r\n]+/g);
+		lines = comment_string.match(/[^\r\n]+/g);
 		line_count = lines.length;
 		line_i = 0;
+
+		var comment = new Comment();
 
 		for (line_i; line_i < line_count; line_i += 1) {
 
@@ -68,32 +117,47 @@ fs.readFile("src/example.js", "utf8", function (err, fileContents) {
 				line = line.substring(1).trim();
 			}
 
-			// Look for keywords in line
 			kw_index.title = line.indexOf("@title");
-			kw_index.author = line.indexOf("@author");
-			kw_index.date = line.indexOf("@date");
-			kw_index.param = line.indexOf("@param");
-			kw_index.return = line.indexOf("@return");
 
 			if ( kw_index.title !== -1 ) {
-				console.log("Title:", line);
+
+				comment.set("title", line);
 			}
+
+			kw_index.author = line.indexOf("@author");
 
 			if ( kw_index.author !== -1 ) {
-				console.log("Author:", line);
+
+				comment.set("author", line);
 			}
+
+			kw_index.date = line.indexOf("@date");
 
 			if ( kw_index.date !== -1 ) {
-				console.log("Date:", line);
+
+				comment.set("date", line);
 			}
+
+			kw_index.param = line.indexOf("@param");
 
 			if ( kw_index.param !== -1 ) {
-				console.log("Param:", line);
+
+				comment.set("param", line);
 			}
 
-			if ( kw_index.return !== -1 ) {
-				console.log("Return:", line);
+			kw_index.return_prop = line.indexOf("@return");
+
+			if ( kw_index.return_prop !== -1 ) {
+
+				comment.set("return_prop", line);
+			}
+
+			if (comment.isValid()) {
+
+				comment.printOut();
 			}
 		}
 	}
+
+	
 });
